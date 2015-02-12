e613f9d97de712953ec29f1105e4385762af7214
Brad Doyle <BDProductionsStudios@gmail.com>
2015-02-12 15:47:54 -0500

    Added all elements to Java script

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var blueberries = {
+	fullName: 'Vaccinium corymbosum',
+	growsOnShrub: true,
+	energy: 240,
+	carboydrates: 14.49,
+	protein: 0.74,
+}
+console.log(blueberries)
+
+var grapes = {
+	fullName: 'Vitis vinifera',
+	growsOnShrub: fulse,
+	energy: 288,
+	carboydrates: 18.1,
+	protein: 0.72,
+}
+console.log(grapes)
+
+var redcurrant = {
+	fullName: 'Vaccinium corymbosum',
+	growsOnShrub: true,
+	energy: 234,
+	carboydrates: 13.8,
+	protein: 1.4,
+}
+console.log(redcurrant)
