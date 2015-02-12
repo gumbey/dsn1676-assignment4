d185b003729f9fb00b40a76756161ec4dc0ec3ee
Brad Doyle <BDProductionsStudios@gmail.com>
2015-02-12 17:09:38 -0500

    Styled all Elements and added a background

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var blueberries = {
 	carboydrates: 14.49,
 	protein: 0.74,
 }
-console.log(blueberries)
 
 var grapes = {
 	fullName: 'Vitis vinifera',
@@ -14,13 +13,31 @@ var grapes = {
 	carboydrates: 18.1,
 	protein: 0.72,
 }
-console.log(grapes)
 
 var redcurrant = {
-	fullName: 'Vaccinium corymbosum',
+	fullName: 'Ribes rubrum',
 	growsOnShrub: true,
 	energy: 234,
 	carboydrates: 13.8,
 	protein: 1.4,
 }
-console.log(redcurrant)
+
+var berries = ['blueberries', 'grapes', 'redcurrant'];
+
+var writeberries = function (berries) {
+	document.write('<h1>' + berries.fullName + '</h1>')
+
+	document.write('<dl>');
+	document.write('<dt>Grows on shrub:</dt>');
+	document.write('<dd>' + berries.growsOnShrub + '</dd>');
+	document.write('<dt>energy:</dt>');
+	document.write('<dd>' + berries.energy + '</dd>');
+	document.write('<dt>Carboydrates:</dt>');
+	document.write('<dd>' + berries.carboydrates + '</dd>');
+	document.write('<dt>Protein:</dt>');
+	document.write('<dd>' + berries.protein + '</dd>');
+	document.write('</dl>');
+}
+
+var allBerries = [blueberries, grapes, redcurrant];
+allBerries.forEach(writeberries);
